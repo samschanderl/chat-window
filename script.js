4f63ae93a4e43b2e3350b0431ca2e00fd5b331b4
Samuel <samuelschanderl@Samuels-MacBook-Pro.local>
2023-01-11 05:22:28 +0100

    changed code into class

--- script.js
+++ script.js
@@ -1,83 +1,113 @@
-function addChatWindow() {
-    // container div
-    const chatContainer = document.createElement('div');
-    chatContainer.classList.add('chat-container');
+export default class ChatWindow {
+    constructor(
+            text={
+                heading: 'Got questions?'
+            }, 
+            colors={
+            heading: '#9700da', 
+            background: '#e9e9e9'}) {
+        this.text = text
+        this.colors = colors
 
-    // chat window
-    const chatWindow = document.createElement('div');
-    chatWindow.classList.add('chat-window');
-    chatContainer.appendChild(chatWindow);
+        this.chatWindowStyles = `
+        @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
+        body {
+            background: #e9e9e9;
+            position: relative;
+            height: 100vh;
+        }
+        .chat-container {
+            font-family: 'Raleway';
+            position: fixed;
+            bottom: 5px;
+            right: 0px;
+            width: 100%;
+            pointer-events: none;
+            z-index: 1000;
+        }
+        .chat-window {
+            background: white;
+            width: 400px;
+            max-width: 30%;
+            padding: 0;
+            margin-left: auto;
+            border-radius: 10px 10px 0 0;
+            pointer-events: auto;
+            box-shadow: 0 0 15px 0 rgba(0,0,0,.1);
+            overflow-y: hidden;
+        }
+        .chat-window:hover {
+            cursor: pointer;
+        }
+        .chat-window__heading {
+            display: flex;
+            align-items: center;
+            margin: 0;
+            padding: 15px;
+            background: ${this.colors.heading};
+            font-size: 30px;
+            color: #f7f7f7;
+        }
+        .chat-icon {
+            margin-left: auto;
+            font-size: 30px;
+        }
+       `
+    }
 
-    // create heading
-    const heading= document.createElement('h2');
-    heading.classList.add('chat-window__heading')
-    heading.innerHTML = `Chat with Us!`;
-    chatWindow.appendChild(heading);
+    addChatWindow() {
+        // container div
+        const chatContainer = document.createElement('div');
+        chatContainer.classList.add('chat-container');
+    
+        // chat window
+        const chatWindow = document.createElement('div');
+        chatWindow.classList.add('chat-window');
+        chatContainer.appendChild(chatWindow);
+    
+        /*
+        #
+        ###### CREATE HEADING ELEMENTS
+        #
+        */
 
-    // create chat icon
-    const iconSpan = document.createElement('span');
-    iconSpan.classList.add('material-symbols-outlined', 'chat-icon');
-    iconSpan.textContent = 'chat';
-    heading.innerHTML += iconSpan.outerHTML
-    console.log(iconSpan)
+        // create heading
+        const heading= document.createElement('h2');
+        heading.classList.add('chat-window__heading')
+        heading.innerHTML = `${this.text.heading}`;
+        chatWindow.appendChild(heading);
+    
+        // create chat icon
+        const iconSpan = document.createElement('span');
+        iconSpan.classList.add('material-symbols-outlined', 'chat-icon');
+        iconSpan.textContent = 'chat';
+        heading.innerHTML += iconSpan.outerHTML
+        console.log(iconSpan)
+    
+        // append final box to the body
+        const script = document.getElementById('chat-js')
+        script.insertAdjacentHTML('beforebegin', chatContainer.outerHTML)
+        console.log(chatContainer)
+    }
+    
+    injectStyles() {
+        const styleTag = document.createElement('style');
+        styleTag.setAttribute('type', 'text/css')
+        styleTag.innerText = this.chatWindowStyles
+        document.head.appendChild(styleTag);
+    }
 
-    // append final box to the body
-    const script = document.getElementById('chat-js')
-    script.insertAdjacentHTML('beforebegin', chatContainer.outerHTML)
-    console.log(chatContainer)
+    printVars() {
+        console.log('these variables are used...')
+        console.log('heading: ', this.text) 
+        console.log('color: ', this.colors)
+    }
+    
+    // run the methods when calling object: inject styles, create chatWindow
+    init() {
+        this.injectStyles();
+        this.addChatWindow();
+        this.printVars();
+    }
+
 }
-
-function injectStyles(styleObj) {
-    const styleTag = document.createElement('style');
-    styleTag.setAttribute('type', 'text/css')
-    styleObjShortened = styleObj
-    styleTag.innerText = styleObjShortened
-    document.head.appendChild(styleTag);
-    console.log(styleTag.innerText)
-    console.log(styleObjShortened)
-}
-
-let chatWindowStyles = `
-    @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
-    body {
-        background: #e9e9e9;
-        position: relative;
-        height: 100vh;
-    }
-    .chat-container {
-        font-family: 'Raleway';
-        position: fixed;
-        bottom: 5px;
-        right: 0px;
-        width: 100%;
-        pointer-events: none;
-        z-index: 1000;
-    }
-    .chat-window {
-        background: white;
-        width: 400px;
-        max-width: 30%;
-        padding: 0;
-        margin-left: auto;
-        border-radius: 10px 10px 0 0;
-        pointer-events: auto;
-        box-shadow: 0 0 15px 0 rgba(0,0,0,.1);
-        overflow-y: hidden;
-    }
-    .chat-window__heading {
-        display: flex;
-        align-items: center;
-        margin: 0;
-        padding: 15px;
-        background: #9700da;
-        font-size: 30px;
-        color: #f7f7f7;
-    }
-    .chat-icon {
-        margin-left: auto;
-        font-size: 30px;
-    }
-    `
-
-injectStyles(chatWindowStyles);
-addChatWindow();
